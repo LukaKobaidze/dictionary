1950cf90e277b19d89d60d51a006412f410ffd53
Luka <lukakobaidze2006@gmail.com>
2023-03-18 17:32:58 +0400

    added keyboard event to Select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { ReactComponent as IconArrowDown } from 'assets/images/icon-arrow-down.svg';
 import { Text, AlertOutsideClick } from 'components';
 import { FontType } from 'App';
@@ -15,7 +15,42 @@ export default function Select(props: Props) {
   const { data, selected, onSelect, className } = props;
 
   const [extended, setExtended] = useState(false);
+  const [itemFocused, setItemFocused] = useState<number | null>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowDown' || e.key === 'Down') {
+        e.preventDefault();
+
+        setItemFocused((state) => {
+          if (state === null) return 0;
+          return Math.min(state + 1, data.length - 1);
+        });
+      } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+        e.preventDefault();
+
+        setItemFocused((state) => {
+          if (state === null) return 0;
+          return Math.max(state - 1, 0);
+        });
+      } else if (e.key === 'Escape') {
+        setExtended(false);
+        buttonRef.current?.focus();
+      }
+    };
+
+    if (extended) {
+      document.addEventListener('keydown', handleKeyDown);
+    } else {
+      setItemFocused(null);
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [extended, data.length]);
 
   return (
     <div className={`${styles.container} ${className || ''}`}>
@@ -40,15 +75,22 @@ export default function Select(props: Props) {
           ignore={[buttonRef]}
           className={styles.extend}
         >
-          {data.map((item) => {
+          {data.map((item, i) => {
             return (
               <button
                 key={item.name}
+                ref={(node) =>
+                  ((itemFocused === null && item.name === selected.name) ||
+                    itemFocused === i) &&
+                  node?.focus()
+                }
                 className={`${styles['extend__button']} ${item.className}`}
                 onClick={(e) => {
                   onSelect(item);
                   setExtended(false);
+                  buttonRef.current?.focus();
                 }}
+                onFocus={() => setItemFocused(i)}
               >
                 {item.name}
               </button>
